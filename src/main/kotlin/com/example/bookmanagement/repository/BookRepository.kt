d66e0af787854f0a3666dc606da501f1582e3f95
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2021-06-18 22:06:55 +0700

    Remove @Param

--- src/main/kotlin/com/example/bookmanagement/repository/BookRepository.kt
+++ src/main/kotlin/com/example/bookmanagement/repository/BookRepository.kt
@@ -4,7 +4,6 @@ import com.example.bookmanagement.model.db.BookEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import javax.transaction.Transactional
 
@@ -13,14 +12,14 @@ import javax.transaction.Transactional
 interface BookRepository: JpaRepository<BookEntity, Int> {
     @Query("select b from BookEntity b where b.isFavourite = :isFavourite and b.status = :status")
     fun findByFavouriteAndStatusCustomQuery(
-        @Param("isFavourite") isFavourite: Boolean,
-        @Param("status") status: String,
+        isFavourite: Boolean,
+        status: String,
     ): List<BookEntity>
 
     @Modifying(flushAutomatically = true, clearAutomatically = true)
     @Query("update BookEntity b set b.isFavourite = :isFavourite where b.id = :id")
     fun updateFavourite(
-        @Param("id") id: Int,
-        @Param("isFavourite") isFavourite: Boolean,
+        id: Int,
+        isFavourite: Boolean,
     )
 }
